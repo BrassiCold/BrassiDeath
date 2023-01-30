c7c768475fc7fa235fcdb87961ef7266061afe5e
BrassiCold <brassicold@outlook.com>
2023-01-30 14:06:59 +0800

    Initial commit

---
+++ src/main/kotlin/fun/brassicold/brassi/death/project/BrassiDeath.kt
@@ -0,0 +1,11 @@
+package `fun`.brassicold.brassi.death.project
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object BrassiDeath : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
